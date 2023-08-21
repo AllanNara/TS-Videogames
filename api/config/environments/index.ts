3ad412835ecf3fed8c236912d7c5143c8d40da0e
AllanNara <allannara@outlook.com>
2023-08-21 19:06:44 -0300

    reset

--- api/config/environments/index.ts
+++ api/config/environments/index.ts
@@ -1,16 +1,10 @@
-
-import DEVELOPMENT from "./development";
-import PRODUCTION from "./production";
-import TEST from "./test";
-
-const { NODE_ENV } = process.env;
-
-let enviroment = DEVELOPMENT;
-
-if(NODE_ENV === 'production') {
-  enviroment = PRODUCTION
-} else if(NODE_ENV === 'test') {
-  enviroment = TEST
+export default {
+	PORT: process.env.PORT,
+	DB: {
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+		host: process.env.DB_HOST,
+		dialect: process.env.DB,
+	},
 };
-
-export default enviroment
--- api/src/models/Videogame.ts
+++ api/src/models/Videogame.ts
@@ -1,10 +1,10 @@
 import {
-  BaseEntity,
-  Column,
-  Entity,
-  PrimaryGeneratedColumn,
-  ManyToMany,
-  JoinTable,
+	BaseEntity,
+	Column,
+	Entity,
+	PrimaryGeneratedColumn,
+	ManyToMany,
+	JoinTable,
 } from "typeorm";
 import { Min, Max } from "class-validator";
 import { Genre } from "./Genre";
@@ -12,51 +12,50 @@ import { Platform } from "./Platform";
 
 @Entity("videogames")
 export class Videogame extends BaseEntity {
-  @PrimaryGeneratedColumn("uuid")
-  id: string;
-  
-  @Column("float", { default: 0, nullable: true })
-  @Max(5)
-  @Min(0)
-  rating: number;
+	@PrimaryGeneratedColumn("uuid")
+	id: string;
 
-  @Column({ unique: true })
-  name: string;
+	@Column("float", { default: 0, nullable: true })
+	@Max(5)
+	@Min(0)
+	rating: number;
 
-  @Column("boolean", { default: true })
-  isDataBase: boolean;
+	@Column({ unique: true })
+	name: string;
 
-  @Column("text", { nullable: true })
-  image: string;
+	@Column("boolean", { default: true })
+	isDataBase: boolean;
 
-  @ManyToMany((type) => Genre, (genre) => genre.videogames)
-  @JoinTable({
-    name: "game_genres",
-    joinColumn: {
-      name: "videogameId",
-    },
-    inverseJoinColumn: {
-      name: "genreId",
-    },
-  })
-  genres: Array<Genre>;
+	@Column("text", { nullable: true })
+	image: string;
 
-  @ManyToMany((type) => Platform, (platform) => platform.videogames)
-  @JoinTable({
-    name: "game_platforms",
-    joinColumn: {
-      name: "videogameId",
-    },
-    inverseJoinColumn: {
-      name: "platformId",
-    },
-  })
-  platforms: Array<Platform>;
+	@ManyToMany((type) => Genre, (genre) => genre.videogames)
+	@JoinTable({
+		name: "game_genres",
+		joinColumn: {
+			name: "videogameId",
+		},
+		inverseJoinColumn: {
+			name: "genreId",
+		},
+	})
+	genres: Array<Genre>;
 
-  @Column({ length: 40, unique: true })
-  description: string;
+	@ManyToMany((type) => Platform, (platform) => platform.videogames)
+	@JoinTable({
+		name: "game_platforms",
+		joinColumn: {
+			name: "videogameId",
+		},
+		inverseJoinColumn: {
+			name: "platformId",
+		},
+	})
+	platforms: Array<Platform>;
 
-  @Column("date", { default: new Date().toISOString().split("T")[0], nullable: true })
-  released: Date;
+	@Column({ length: 40, unique: true })
+	description: string;
 
+	@Column("date", { default: new Date().toISOString().split("T")[0], nullable: true })
+	released: Date;
 }
